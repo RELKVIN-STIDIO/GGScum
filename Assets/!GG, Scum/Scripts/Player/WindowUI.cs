1ed469f1c507a0127406bc43bd12519e91e21ba1
RECH-afk <185964059+ic3nt@users.noreply.github.com>
2025-07-24 18:42:46 +0300

    HI IM NAVALNЫЙ

--- Assets/!GG, Scum/Scripts/Player/WindowUI.cs
+++ Assets/!GG, Scum/Scripts/Player/WindowUI.cs
@@ -1,19 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class WindowUI : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
-    Vector3 MouseDragStartPos;
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private RectTransform canvasRect;
+    private Vector2 mouseOffset;
 
-    public void OnDrag(PointerEventData eventData)
+    void Awake()
     {
-        transform.localPosition = Input.mousePosition - MouseDragStartPos;
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasRect = canvas.GetComponent<RectTransform>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        MouseDragStartPos = Input.mousePosition - transform.localPosition;
+        // Вычисляем смещение курсора относительно центра объекта
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out mouseOffset);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRect,
+            eventData.position,
+            eventData.pressEventCamera,
+            out Vector2 localPoint))
+        {
+            // Учитываем смещение курсора и ограничиваем позицию в пределах Canvas
+            rectTransform.localPosition = ClampToCanvas(localPoint - mouseOffset);
+        }
+    }
+
+    private Vector2 ClampToCanvas(Vector2 position)
+    {
+        // Получаем размеры объекта и Canvas
+        Vector2 size = rectTransform.sizeDelta * canvas.scaleFactor;
+        Vector2 canvasSize = canvasRect.sizeDelta;
+
+        // Ограничиваем позицию, чтобы объект не выходил за границы Canvas
+        float xMin = -canvasSize.x * 0.5f + size.x * 0.5f;
+        float xMax = canvasSize.x * 0.5f - size.x * 0.5f;
+        float yMin = -canvasSize.y * 0.5f + size.y * 0.5f;
+        float yMax = canvasSize.y * 0.5f - size.y * 0.5f;
+
+        position.x = Mathf.Clamp(position.x, xMin, xMax);
+        position.y = Mathf.Clamp(position.y, yMin, yMax);
+
+        return position;
     }
 }
